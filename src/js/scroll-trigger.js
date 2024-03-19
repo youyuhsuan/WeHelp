debe699ff2da8c1cec767a05ba2b5874a34e0989
youyuhsuan <stellayou0118@gmail.com>
2024-03-19 10:56:30 +0800

    chore:article content feat:animation

---
+++ src/js/scroll-trigger.js
@@ -0,0 +1,74 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".sc_1", {
+  x: 20,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".sc_1",
+    start: "top 60%",
+    end: "center 20%",
+  },
+});
+gsap.to(".sc_2", {
+  x: 20,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".sc_2",
+    start: "top 60%",
+    end: "center 20%",
+  },
+});
+gsap.to(".sc_3", {
+  x: 20,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".sc_3",
+    start: "top 60%",
+    end: "center 20%",
+  },
+});
+gsap.to(".sc_4", {
+  x: 20,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".sc_4",
+    start: "top 60%",
+    end: "center 20%",
+  },
+});
+gsap.to(".sc_5", {
+  x: 20,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".sc_5",
+    start: "top 60%",
+    end: "center 20%",
+  },
+});
+gsap.to(".sc_6", {
+  x: 20,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".sc_6",
+    start: "top 60%",
+    end: "center 20%",
+  },
+});
+gsap.to(".sc_7", {
+  x: 20,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".sc_7",
+    start: "top 60%",
+    end: "center 20%",
+  },
+});
+gsap.to(".sc_8", {
+  x: 20,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".sc_8",
+    start: "top 60%",
+    end: "center 20%",
+  },
+});
